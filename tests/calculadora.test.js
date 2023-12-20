f53eef479003bd4334b1a11246c135bc20b0103b
marceloatp <59808346+marceloatp@users.noreply.github.com>
2023-12-20 14:54:28 +0000

    Criado teste de teste automatizado

---
+++ tests/calculadora.test.js
@@ -0,0 +1,9 @@
+test("nome do teste", () => console.log("E agora funciona?"));
+
+function callbackFuntion() {
+  console.log("esta mensagem esta a ser chamada?");
+}
+
+test("testando outra condiçao do sistema", () => {
+  console.log("outro teste do sistema");
+})
